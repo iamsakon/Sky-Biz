77d729a27f2b3ab365874ec5e76e527c07027658
iamsakon <sakon.bluechip@gmail.com>
2016-06-21 18:45:31 +0700

    prodim

--- sky-biz-develop/sky-biz-parent/sky-biz-webpf/src/main/java/com/sky/biz/mbean/TreeMenuView.java
+++ sky-biz-develop/sky-biz-parent/sky-biz-webpf/src/main/java/com/sky/biz/mbean/TreeMenuView.java
@@ -26,24 +26,32 @@ public class TreeMenuView {
 	@PostConstruct
 	public void init() {
 		root = new DefaultTreeNode("Root", null);
-		TreeNode node0 = new DefaultTreeNode(new MenuItemDto("System",""), root);
-		TreeNode node1 = new DefaultTreeNode(new MenuItemDto("E-HRM",""), root);
+		TreeNode node0 = this.getRootTreeNode(root,"System","");
+		TreeNode node1 = this.getRootTreeNode(root, "E-HRM", "");
+		TreeNode node2 = this.getRootTreeNode(root, "Inventory", "");
 
 		TreeNode node00 = new DefaultTreeNode(new MenuItemDto("General",""), node0);
 		TreeNode node01 = new DefaultTreeNode(new MenuItemDto("Structure",""), node0);
 
 		TreeNode node10 = new DefaultTreeNode(new MenuItemDto("OM",""), node1);
-		TreeNode node20 = new DefaultTreeNode(new MenuItemDto("PIM",""), node1);
+		TreeNode node11 = new DefaultTreeNode(new MenuItemDto("PIM",""), node1);
+		
+		TreeNode node20 = this.getRootTreeNode(node2,"Product Information","");
+		TreeNode node21 = this.getRootTreeNode(node2,"UOM","");
 
 		node1.getChildren().add(new DefaultTreeNode(new MenuItemDto("Node 1.1","")));
 		node00.getChildren().add(new DefaultTreeNode(new MenuItemDto("Node 0.0.0","app/org/general")));
 		node00.getChildren().add(new DefaultTreeNode(new MenuItemDto("Node 0.0.1","app/org/structure")));
 		node01.getChildren().add(new DefaultTreeNode(new MenuItemDto("Node 0.1.0","")));
 		node10.getChildren().add(new DefaultTreeNode(new MenuItemDto("Node 1.0.0","")));
-		root.getChildren().add(new DefaultTreeNode(new MenuItemDto("Node 2","")));
+		
+		this.getRootTreeNode(node20,"view","app/product-info/product-carousel");
 	}
 
 
+	private TreeNode getRootTreeNode(TreeNode parent,String menuName,String outcome){
+		return new DefaultTreeNode(new MenuItemDto(menuName,outcome),parent);
+	}
 	
 	public TreeNode getRoot() {
 		return root;
